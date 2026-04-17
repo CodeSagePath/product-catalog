34ce42d1ccb4f633567f89d68301d81181614ea9
CodeSagePath <rishigarg185@gmail.com>
2026-04-17 12:59:09 +0530

    Updates:   - Added mongoose as a dependency.   - Added logic to connect to mongodb database running locally.

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-// use es6 module loader
-
-// enable cors
-
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -12,10 +9,18 @@ app.use(cors());
 
 const PORT = 3344;
 
+mongoose.connect("mongodb://127.0.0.1/product-catalog")
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB", err.message);
+  });
+
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Product Catalog API' });
+  res.json({ message: "Welcome to the Product Catalog API" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
 });
